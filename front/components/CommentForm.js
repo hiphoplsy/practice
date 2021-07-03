9de7ec801e9bc4ef2d73673353de691e08dcf272
hiphoplsy <hiphoplsy@naver.com>
2021-07-03 12:58:55 +0900

    ADD: CommentForm

--- front/components/CommentForm.js
+++ front/components/CommentForm.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
+import { Form, Input, Button } from 'antd';
 
 const CommentForm = ({ post }) => {
+  const [commentText, setCommentText] = useState('');
+
+  const onSubmitComment = useCallback(() => {
+    console.log(commentText);
+  }, [commentText]);
+
+  const onChangeCommentText = useCallback((e) => {
+    setCommentText(e.target.value);
+    setCommentText('');
+  }, []);
+
   return (
-    <div>CommentForm</div>
+    <Form onFinish={onSubmitComment}>
+      <Form.Item style={{ position: 'relative', margin: 0 }}>
+        <Input.TextArea row={4} maxLength={300} value={commentText} onChange={onChangeCommentText} />
+        <Button style={{ position: 'absolute', right: 0, bottom: -40 }} type="primary" htmlType="submit">댓글삐약</Button>
+      </Form.Item>
+    </Form>
   )
 };
 
