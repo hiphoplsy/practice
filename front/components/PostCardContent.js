042eb8fc753aabcb6dc7e9c924152067aa774e2e
hiphoplsy <hiphoplsy@naver.com>
2021-07-05 13:30:19 +0900

    ADD: hashtag

--- front/components/PostCardContent.js
+++ front/components/PostCardContent.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Link from 'next/link';
 import PropTypes from 'prop-types';
 
 const PostCardContent = ({ postData }) => {
   return (
-    <div>PostCardContent</div>
+    <div>
+      {postData.split(/(#[^\s#]+)/g).map((v) => {
+        if (v.match(/(#[^\s#]+)/g)) {
+          return (
+          <Link
+            href={{ pathname: '/hashtag', query: { tag: v.slice(1)} }}
+            as={`/hashtag/${v.slice(1)}`}
+            key={v}
+          >
+            <a>{v}</a>
+          </Link>
+          );
+        }
+        return v;
+      })}
+    </div>
   )
 };
 
